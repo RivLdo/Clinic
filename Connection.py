4098d98b70b721f8802f2fd566249dd85d0a0b78
RivLdo <116156433+RivLdo@users.noreply.github.com>
2023-12-19 12:45:52 +0700

    Update Connection.py

--- Connection.py
+++ Connection.py
@@ -3,10 +3,7 @@ import pymysql.cursors
 def conn_to_database():
     try:
         conn = pymysql.connect(
-            host='localhost',
-            user='root',
-            password='arrivaldophpMyAdmin-0',
-            database='merge',
+            # 
             cursorclass=pymysql.cursors.DictCursor
         )
         return conn
